cfae82c7b02404cac89a2d286f72f62f6c9fef8c
Nick Breslin <nickbreslin@gmail.com>
2018-10-01 22:01:42 -0700

    Adding grunt

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg:        grunt.file.readJSON('package.json'),
+        jshint:     grunt.file.readJSON('grunt/jshint.json'),
+        uglify:     grunt.file.readJSON('grunt/uglify.json'),
+        clean:      grunt.file.readJSON('grunt/clean.json'),
+        watch:      grunt.file.readJSON('grunt/watch.json'),
+        cssmin:     grunt.file.readJSON('grunt/cssmin.json'),
+        handlebars: grunt.file.readJSON('grunt/handlebars.json'),
+        concat:     grunt.file.readJSON('grunt/concat.json'),
+        copy:       grunt.file.readJSON('grunt/copy.json')
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('setup',          ['concat:lib', 'concat:libcss', 'copy']);
+    grunt.registerTask('compile-assets', ['cssmin',     'handlebars']);
+
+    grunt.registerTask('compile-common', ['clean:common', 'jshint:js-common', 'concat:js-common']);
+    grunt.registerTask('compile-app',    ['clean:app',    'jshint:js-app',    'concat:js-app']);
+
+    grunt.registerTask('default',        ['setup', 'compile-assets', 'compile-common', 'compile-app']);
+    grunt.registerTask('build',          ['clean', 'setup', 'compile-assets', 'concat', 'uglify']);
+};
